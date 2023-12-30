e8820147efe04ecb0cff1f352d487274f682657d
Dominik Hucka <dominikhucka@googlemail.com>
2023-12-30 16:18:01 +0100

    finish

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,29 +1,3 @@
-// function fullscreen() {
-//     let fullscreen = document.getElementById('fullScreen');
-//     enterFullscreen(fullscreen);
-// }
-
-
-
-
-// function enterFullscreen(element) {
-//     if (element.requestFullscreen) {
-//         element.requestFullscreen();
-//     } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
-//         element.msRequestFullscreen();
-//     } else if (element.webkitRequestFullscreen) {  // iOS Safari
-//         element.webkitRequestFullscreen();
-//     }
-// }
-
-
-// function exitFullscreen() {
-//     if (document.exitFullscreen) {
-//         document.exitFullscreen();
-//     } else if (document.webkitExitFullscreen) {
-//         document.webkitExitFullscreen();
-//     }
-// }
 /**
  * Toggles the fullscreen mode for the game.
  */
